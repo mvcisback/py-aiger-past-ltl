dddea38b09881c22a36a9111d5f7e3c84e935baf
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-10-24 11:03:55 -0700

    move code out of __init__ and make test file.

---
+++ aiger_ptltl/ptltl.py
@@ -0,0 +1,136 @@
+from uuid import uuid1
+
+import aiger
+import funcy as fn
+from lenses import bind
+
+from parsimonious import Grammar, NodeVisitor
+import hypothesis.strategies as st
+from hypothesis_cfg import ContextFreeGrammarStrategy
+
+
+PLTL_GRAMMAR = Grammar(u'''
+phi =  since / or / and / implies / hist / past / vyest / neg / AP
+or = "(" _ phi _ "|" _ phi _ ")"
+implies = "(" _ phi _ "->" _ phi _ ")"
+and = "(" _ phi _ "&" _ phi _ ")"
+hist = "H" _ phi
+past = "P" _ phi
+vyest = "Z" _ phi
+since = "[" _ phi _ "S" _ phi _ "]"
+neg = "~" _ phi
+
+_ = ~r" "*
+AP = ~r"[a-zA-z]" ~r"[a-zA-Z\d]*"
+EOL = "\\n"
+''')
+
+
+class PTLTLExpr(aiger.BoolExpr):
+    def __call__(self, trc):
+        if isinstance(trc, list):
+            val, _ = self.aig.simulate(trc)[-1]
+            return val[self.output]
+        else:
+            return aiger.BoolExpr.__call__(self, trc)
+
+    def historically(self):
+        return PTLTLExpr(self.aig >> hist_monitor(self.output))
+
+    def once(self):
+        return PTLTLExpr(self.aig >> past_monitor(self.output))
+
+    def vyest(self):
+        return PTLTLExpr(self.aig >> vyest_monitor(self.output))
+
+    def since(self, other):
+        monitor = since_monitor(self.output, other.output)
+        return PTLTLExpr((self.aig | other.aig) >> monitor)
+
+
+class PLTLVisitor(NodeVisitor):
+    def generic_visit(self, _, children):
+        return children
+
+    def visit_phi(self, _, children):
+        return children[0]
+
+    def visit_AP(self, node, _):
+        return PTLTLExpr(aiger.atom(node.text).aig)
+
+    def visit_and(self, _, children):
+        return children[2] & children[6]
+
+    def visit_or(self, _, children):
+        return children[2] | children[6]
+
+    def visit_neg(self, _, children):
+        return ~children[2]
+
+    def visit_implies(self, _, children):
+        return children[2].implies(children[6])
+
+    def visit_vyest(self, _, children):
+        return children[2].vyest()
+
+    def visit_hist(self, _, children):
+        return children[2].historically()
+
+    def visit_past(self, _, children):
+        return children[2].once()
+    
+    def visit_since(self, _, children):
+        return children[2].since(children[6])
+
+
+def vyest_monitor(name):
+    return aiger.delay(
+        inputs=[name],
+        initials=[True],
+        latches=[aiger.common._fresh()],
+        outputs=[aiger.common._fresh()]
+    )
+
+
+def hist_monitor(name):
+    out = aiger.common._fresh()
+    return aiger.and_gate([name, 'tmp'], out).feedback(
+        inputs=['tmp'],
+        outputs=[out],
+        latches=[aiger.common._fresh()],
+        initials=[True],
+        keep_outputs=True
+    )
+
+
+def past_monitor(name):
+    out = aiger.common._fresh()
+    return aiger.or_gate([name, 'tmp'], out).feedback(
+        inputs=['tmp'],
+        outputs=[out],
+        latches=[aiger.common._fresh()],
+        initials=[False],
+        keep_outputs=True
+    )
+
+
+def since_monitor(left, right):
+    circ = aiger.and_gate([left, 'active'], 'active')
+    tmp = aiger.common._fresh()
+    circ >>= aiger.or_gate([right, 'active'], tmp)
+    return circ.feedback(
+        inputs=['active'],
+        outputs=[tmp],
+        latches=[aiger.common._fresh()],
+        initials=[True],
+        keep_outputs=True,
+    )
+
+
+def parse(pltl_str: str, output=None):
+    expr = PLTLVisitor().visit(PLTL_GRAMMAR.parse(pltl_str))
+    aig = expr.aig.evolve(comments=(pltl_str,))
+    if output is not None:
+        aig = aig['o', {expr.output: output}]
+
+    return type(expr)(aig)
---
+++ aiger_ptltl/test_ptltl.py
@@ -0,0 +1,56 @@
+import hypothesis.strategies as st
+from hypothesis import given
+from hypothesis_cfg import ContextFreeGrammarStrategy
+
+import aiger_ptltl
+
+GRAMMAR = {
+    'phi': (
+        ('Unary', 'phi'),
+        ('(', 'phi', 'Binary', 'phi', ')'),
+        ('[', 'phi', ' S ', 'phi', ']'),
+        ('AP', ),
+    ),
+    'Unary': (('~', ), ('P',), ('H',), ('Z', )),
+    'Interval': (('', ), ('[1, 3]', )),
+    'Binary': (
+        (' | ', ), (' & ', ), (' -> ', ),
+    ),
+    'AP': (('ap1', ), ('ap2', ), ('ap3', ), ('ap4', ), ('ap5', )),
+}
+
+PTLTL_STRATEGY = st.builds(
+    lambda term: aiger_ptltl.parse(''.join(term)),
+    ContextFreeGrammarStrategy(GRAMMAR, max_length=14, start='phi')
+)
+
+
+TRUE = {'ap1': True, 'ap2': True, 'ap3': True, 'ap4': True, 'ap5': True}
+VALUE_STRATEGY = st.fixed_dictionaries({
+    'ap1': st.booleans(),
+    'ap2': st.booleans(),
+    'ap3': st.booleans(),
+    'ap4': st.booleans(),
+    'ap5': st.booleans(),
+})
+TRACE_STRATEGY = st.lists(VALUE_STRATEGY, min_size=1)
+
+
+@given(PTLTL_STRATEGY, VALUE_STRATEGY)
+def test_smoke_value_eval(expr, val):
+    res = expr(val)
+    assert isinstance(res, bool)
+
+
+@given(PTLTL_STRATEGY, PTLTL_STRATEGY, TRACE_STRATEGY)
+def test_hist_identity(expr1, expr2, trc):
+    expr3 = expr1.historically() & expr2.historically()
+    expr4 = (expr1 & expr2).historically()
+    assert expr3(trc) == expr4(trc)
+
+
+@given(PTLTL_STRATEGY, PTLTL_STRATEGY, TRACE_STRATEGY)
+def test_past_identity(expr1, expr2, trc):
+    expr3 = expr1.historically() & expr2.historically()
+    expr4 = (expr1 & expr2).historically()
+    assert expr3(trc) == expr4(trc)
